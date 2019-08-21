29b8593280a32463c966de2fd53e352b48c92390
AbdullahAl-Hajjar <46487867+AbdullahAl-Hajjar@users.noreply.github.com>
2019-08-21 18:51:00 -0400

    Add files via upload

---
+++ ChatApplication.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace ChatApplication
+{
+    class ChatApplication
+    {
+        static void Main(string[] args)
+        {
+
+            TcpListener server = new TcpListener(IPAddress.Any, 8088);
+            TcpClient client = default(TcpClient);
+
+     
+
+            try
+            {
+                server.Start();
+                Console.WriteLine("Server Strating!");
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                Console.Read();
+            }
+
+            while(true)
+            {
+                client= server.AcceptTcpClient();
+
+                byte[] unit = new byte[500];
+                NetworkStream stream = client.GetStream();
+
+                stream.Read(unit, 0, unit.Length);
+                string msg = null;
+                foreach (byte b in unit)
+                {
+                    if (b != 0)
+                        msg += Convert.ToChar(b);
+                }
+
+                Console.WriteLine(msg);
+                
+
+
+            }
+        }
+    }
+}
---
+++ Client.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    public partial class Client : Form
+    {
+        public Client()
+        {
+            InitializeComponent();
+        }
+
+        IPAddress ip = IPAddress.Parse("127.0.0.1");
+        
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TcpClient client = new TcpClient();
+            client.Connect(ip, 8088);
+            NetworkStream server = client.GetStream();
+            int count = Encoding.ASCII.GetByteCount(message.Text);
+            byte[] outStream = new byte[count];
+            outStream = System.Text.Encoding.ASCII.GetBytes(message.Text);
+            server.Write(outStream, 0, outStream.Length);
+            server.Flush();
+            server.Close();
+
+        }
+
+    }
+}
